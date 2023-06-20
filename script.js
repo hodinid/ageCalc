1c87cd744120cb363ec338730acad531f8f15d9f
hodinid <79570078+hodinid@users.noreply.github.com>
2023-06-20 16:24:26 +0500

    some details

--- script.js
+++ script.js
@@ -1,55 +1,70 @@
-const months = [ 31,28,31,30,31,30,31,31,30,31,30,31 ];
-function myAge(){
-    ageCalculate(true)
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+function myAge() {
+    ageCalculate(`myBirth`)
 }
-function ageCalculate(me){
+function until() {
+    ageCalculate(`untilDown`)
+    console.log('hello');
+}
+function ageCalculate(me) {
     let today = new Date();
-    let inputDate = new Date(document.getElementById('input-date').value);
-    if (me){
-        document.getElementById('input-date').value = '2005-01-24';
-        inputDate = new Date('2005-01-24');
-    }
-    let birthMonth, birthDate, birthYear;
-
-    let birthDetails = {
-        date: inputDate.getDate(),
-        month: inputDate.getMonth()+1,
-        year: inputDate.getFullYear()
-    }
-    let currentYear = today.getFullYear();
-    let currentMonth = today.getMonth()+1;
-    let currentDate = today.getDate();
-
-    leapChecker(currentYear);
-
-    birthYear =  currentYear-birthDetails.year;
-    if (currentMonth>=birthDetails.month){
-        birthMonth = currentMonth-birthDetails.month;
-    } else {
-        birthYear--;
-        birthMonth = 12 + currentMonth - birthDetails.month;
-    }
-    if(currentDate >= birthDetails.date){
-        birthDate = currentDate - birthDetails.date;
-    } else {
-        birthMonth--;
-        let days = months[currentMonth-2];
-        birthDate = days + currentDate - birthDetails.date;
-        if (birthMonth < 0){
-            birthMonth = 11;
-            birthYear--;
+    if (me == `untilDown`) {
+        untilDate = Date.parse(`2024-06-20`);
+        todayDate = Date.parse(today);
+        untilDays = Math.floor((untilDate-todayDate)/1000/60/60/24);
+        if (untilDays > 0){
+            document.querySelector('.untilDays').textContent = untilDays;
+        } else {
+            document.querySelector('.untilDays').textContent = 0;
         }
+    } else {
+        let inputDate = new Date(document.getElementById('input-date').value);
+        if (me == `myBirth`) {
+            document.getElementById('input-date').value = '2005-01-24';
+            inputDate = new Date('2005-01-24');
+        }
+        let birthMonth, birthDate, birthYear;
+
+        let birthDetails = {
+            date: inputDate.getDate(),
+            month: inputDate.getMonth() + 1,
+            year: inputDate.getFullYear()
+        }
+        let currentYear = today.getFullYear();
+        let currentMonth = today.getMonth() + 1;
+        let currentDate = today.getDate();
+
+        leapChecker(currentYear);
+
+        birthYear = currentYear - birthDetails.year;
+        if (currentMonth >= birthDetails.month) {
+            birthMonth = currentMonth - birthDetails.month;
+        } else {
+            birthYear--;
+            birthMonth = 12 + currentMonth - birthDetails.month;
+        }
+        if (currentDate >= birthDetails.date) {
+            birthDate = currentDate - birthDetails.date;
+        } else {
+            birthMonth--;
+            let days = months[currentMonth - 2];
+            birthDate = days + currentDate - birthDetails.date;
+            if (birthMonth < 0) {
+                birthMonth = 11;
+                birthYear--;
+            }
+        }
+        displayResult(birthDate, birthMonth, birthYear);
     }
-    displayResult(birthDate, birthMonth, birthYear);
 }
 
-function leapChecker(year){
-    if (year%4==0 || (year%100 == 0 && year%400==0)){
-        months[1]=29;
+function leapChecker(year) {
+    if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0)) {
+        months[1] = 29;
     }
 };
 
-function displayResult(bDate, bMonth, bYear){
+function displayResult(bDate, bMonth, bYear) {
     document.querySelector("#years").textContent = bYear;
     document.querySelector("#months").textContent = bMonth;
     document.querySelector("#days").textContent = bDate;
